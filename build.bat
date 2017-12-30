9c847eba94ad37b6f1319bb645e2eadaede260e4
Paul Welter <pwelter@loresoft.com>
2017-12-30 00:59:05 -0600

    add database to build

--- build.bat
+++ build.bat
@@ -1,5 +1,6 @@
 @echo off
 
+@echo *** Build Client ***
 pushd %APPVEYOR_BUILD_FOLDER%\client
 
 call npm install aurelia-cli -g
@@ -8,7 +9,12 @@ call au build --env prod
 
 popd
 
+@echo *** Build Database ***
+msbuild %APPVEYOR_BUILD_FOLDER%\database\InstructorIQ.sln /t:Build /p:Configuration=Release  /p:OutputPath=%APPVEYOR_BUILD_FOLDER%
+
+@echo *** Build Service ***
 dotnet publish %APPVEYOR_BUILD_FOLDER%\service\src\InstructorIQ.Web\InstructorIQ.Web.csproj -c Release -o %APPVEYOR_BUILD_FOLDER%\publish 
 
+@echo *** Zip Packages ***
 7z a InstructorIQ.Client.v%APPVEYOR_BUILD_VERSION%.zip %APPVEYOR_BUILD_FOLDER%\client\dist\*
 7z a InstructorIQ.Service.v%APPVEYOR_BUILD_VERSION%.zip %APPVEYOR_BUILD_FOLDER%\publish\*
