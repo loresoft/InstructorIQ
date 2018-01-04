8fd12e8306334adc6ae6e94a715578fc7250ec75
Paul Welter <pwelter@loresoft.com>
2018-01-04 10:01:15 -0600

    more data deploy tweaks

--- deploy/DbDeploy.ps1
+++ deploy/DbDeploy.ps1
@@ -4,7 +4,7 @@ try
     $publishFile = Resolve-Path publish.config
 
     #Register DLL, SQL Server 2017
-    Add-Type -Path "${env:ProgramFiles(x86)}\Microsoft SQL Server\130\DAC\bin\Microsoft.SqlServer.Dac.dll" 
+    Add-Type -Path "${env:ProgramFiles(x86)}\Microsoft SQL Server\140\DAC\bin\Microsoft.SqlServer.Dac.dll" 
 
     Write-Host "Profile: $publishFile" 
     #Read publish profile XML to get the deployment options
@@ -14,7 +14,8 @@ try
     $dacService = New-Object Microsoft.SqlServer.dac.dacservices($dacProfile.TargetConnectionString)
 
     # register events
-    register-objectevent -in $dacService -eventname Message -source "message" -action { Write-Host $Event.SourceArgs[1].Message.Message }
+    Register-ObjectEvent -InputObject $dacService -EventName "ProgressChanged" -SourceIdentifier "ProgressChanged" -Action { Write-Verbose ("DacServices: {0}" -f $EventArgs.Message) } | Out-Null
+    Register-ObjectEvent -InputObject $dacService -EventName "Message" -SourceIdentifier "Message" -Action { Write-Host ($EventArgs.Message.Message) } | Out-Null
 
     Write-Host "Package: $packFile" 
     $dacPackage = [Microsoft.SqlServer.Dac.DacPackage]::Load($packFile)
@@ -24,10 +25,12 @@ try
 }
 catch
 {
+    Write-Error $PSItem.ToString()
     exit -1
 }
 finally
 {
     # clean up event
-    unregister-event -source "message" -ErrorAction SilentlyContinue
+    Unregister-Event -SourceIdentifier "ProgressChanged" -ErrorAction SilentlyContinue
+    Unregister-Event -SourceIdentifier "Message" -ErrorAction SilentlyContinue
 }
