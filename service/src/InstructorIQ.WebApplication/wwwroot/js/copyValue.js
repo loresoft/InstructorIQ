0af458ba90c37842a94736188e43b4b3ef68ca9c
Paul Welter <pwelter@loresoft.com>
2019-03-15 23:32:45 -0500

    add copy value to start date

---
+++ service/src/InstructorIQ.WebApplication/wwwroot/js/copyValue.js
@@ -0,0 +1,23 @@
+﻿(function ($) {
+
+    $.fn.copyValue = function () {
+
+        this.blur(function () {
+            var currentValue = this.value;
+            if (!currentValue)
+                return;
+
+            var target = $(this).data('target');
+            var $target = $(target);
+
+            var targetValue = $target.val();
+            if (targetValue)
+                return;
+
+            $target.val(currentValue);
+        });
+
+        return this;
+    };
+
+}(jQuery));
