0b3dc08cdcecb06d4d4140ff5ba3758d3656954e
Paul Welter <pwelter@loresoft.com>
2019-12-22 15:07:22 -0600

    remember topic query

--- service/src/InstructorIQ.Core/Extensions/TypeExtensions.cs
+++ service/src/InstructorIQ.Core/Extensions/TypeExtensions.cs
@@ -7,6 +7,39 @@ namespace InstructorIQ.Core.Extensions
     public static class TypeExtensions
     {
         /// <summary>
+        /// Gets the underlying type dealing with <see cref="T:Nullable`1"/>.
+        /// </summary>
+        /// <param name="type">The type.</param>
+        /// <returns>Returns a type dealing with <see cref="T:Nullable`1"/>.</returns>
+        public static Type GetUnderlyingType(this Type type)
+        {
+            if (type == null)
+                throw new ArgumentNullException("type");
+
+            Type t = type;
+            bool isNullable = t.IsGenericType && (t.GetGenericTypeDefinition() == typeof(Nullable<>));
+            if (isNullable)
+                return Nullable.GetUnderlyingType(t);
+
+            return t;
+        }
+
+        /// <summary>
+        /// Determines whether the specified <paramref name="type"/> can be null.
+        /// </summary>
+        /// <param name="type">The type to check.</param>
+        /// <returns>
+        ///   <c>true</c> if the specified <paramref name="type"/> can be null; otherwise, <c>false</c>.
+        /// </returns>
+        public static bool IsNullable(this Type type)
+        {
+            if (!type.IsValueType)
+                return true;
+
+            return type.IsGenericType && (type.GetGenericTypeDefinition() == typeof(Nullable<>));
+        }
+
+        /// <summary>
         /// Determines whether the specified type implements an interface.
         /// </summary>
         /// <typeparam name="TInterface">The type of the interface.</typeparam>
--- service/src/InstructorIQ.WebApplication/Pages/Topic/Index.cshtml.cs
+++ service/src/InstructorIQ.WebApplication/Pages/Topic/Index.cshtml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Threading.Tasks;
 using MediatR.CommandQuery.Queries;
 using InstructorIQ.Core.Domain.Models;
 using InstructorIQ.Core.Extensions;
@@ -18,17 +19,27 @@ namespace InstructorIQ.WebApplication.Pages.Topic
         {
             Sort = nameof(TopicReadModel.TargetMonth);
             Selected = new List<Guid>();
-            Year = DateTime.Now.Year;
         }
 
         [BindProperty(Name = "year", SupportsGet = true)]
-        public int Year { get; set; }
+        public int? Year { get; set; }
 
         [BindProperty(Name = "month", SupportsGet = true)]
-        public int Month { get; set; }
+        public int? Month { get; set; }
 
         [BindProperty]
         public List<Guid> Selected { get; set; }
+
+        public override async Task<IActionResult> OnGetAsync()
+        {
+            ReadHistory();
+
+            var result = await base.OnGetAsync();
+
+            WriteHistory();
+
+            return result;
+        }
 
         public IActionResult OnPostAddGroups()
         {
@@ -57,6 +68,7 @@ namespace InstructorIQ.WebApplication.Pages.Topic
             return RedirectToPage("/Topic/Multiple", new { TopicIds = Selected, tenant = TenantRoute });
         }
 
+
         protected override EntityQuery CreateQuery()
         {
             var query = new EntityQuery(null, PageNumber, PageSize, Sort);
@@ -81,8 +93,12 @@ namespace InstructorIQ.WebApplication.Pages.Topic
             return filter;
         }
 
+
         private void AddYearFilter(List<EntityFilter> filters)
         {
+            if (Year == null || Year == 0)
+                Year = DateTime.Now.Year;
+
             var yearFilter = new EntityFilter
             {
                 Name = nameof(TopicReadModel.CalendarYear),
@@ -94,7 +110,7 @@ namespace InstructorIQ.WebApplication.Pages.Topic
 
         private void AddMonthFilter(List<EntityFilter> filters)
         {
-            if (Month <= 0)
+            if (Month.HasValue == false || Month.Value <= 0)
                 return;
 
             var monthFilter = new EntityFilter
@@ -132,6 +148,37 @@ namespace InstructorIQ.WebApplication.Pages.Topic
                 }
             };
             filters.Add(textFilter);
+        }
+
+
+        private void WriteHistory()
+        {
+            Response.Cookies.Append("_topic_year", Year?.ToString() ?? string.Empty);
+            Response.Cookies.Append("_topic_month", Month?.ToString() ?? string.Empty);
+            Response.Cookies.Append("_topic_query", Query ?? string.Empty);
+        }
+
+        private void ReadHistory()
+        {
+            if (Year == null || Year == 0)
+                Year = ReadCookieInt32("_topic_year") ?? DateTime.Now.Year;
+
+            if (Month == null)
+                Month = ReadCookieInt32("_topic_month");
+
+            if (!Request.Query.ContainsKey("q") && Request.Cookies.TryGetValue("_topic_query", out var queryCookie))
+                Query = queryCookie?.Trim();
+        }
+
+        private int? ReadCookieInt32(string name)
+        {
+            if (!Request.Cookies.TryGetValue(name, out var cookie))
+                return null;
+
+            if (!int.TryParse(cookie, out var value))
+                return null;
+
+            return value;
         }
     }
 }
